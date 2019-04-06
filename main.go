fffcf351466b6c7a16264c0c52ced1c948c2cfca
Eve-GUO <zguo19hawk@gmail.com>
2019-04-06 15:21:52 -0500

    Serve static directory

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fs := http.FileServer(http.Dir("static/"))
-		http.Handle("/static/", http.StripPrefix("/static/", fs))
+		http.StripPrefix("/static/", http.FileServer(http.Dir("/static")))
 	})
 	http.HandleFunc("/123", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to the root123")
